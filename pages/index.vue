415c68943eb90d82c02bdcea28a91c20f44f26fb
younes <younes.barrad@77diamonds.com>
2022-04-09 23:49:46 +0000

    🚀 updated style

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,17 @@
 <template>
-  <div class="flex justify-center flex-col text-center mt-24">
+  <div class="flex flex-col justify-center px-4 mt-24 text-center">
     <div>
-      <fa class="text-blue-400 text-9xl" :icon="['fab', 'github']" />
+      <fa
+        class="text-blue-400 text-8xl md:text-9xl"
+        :icon="['fab', 'github']"
+      />
     </div>
-    <h1 class="font-serif font-normal text-3xl mt-20">
+    <h1 class="mt-20 font-serif text-xl font-normal md:text-3xl">
       Find Your Github Profile
     </h1>
     <form v-on:submit.prevent="onSubmit">
       <input
-        class="
-          mt-10
-          bg-blueGray-700
-          focus:outline-none focus:ring focus:ring-blueGray-500
-          rounded-lg
-          p-4
-          outline-none
-          text-center
-          font-normal
-          text-2xl text-blue-400
-          w-4/5
-          md:w-2/4
-        "
+        class="w-full p-4 mt-10 text-2xl font-normal text-center text-blue-400 rounded-lg outline-none  bg-blueGray-700 focus:outline-none focus:ring focus:ring-blueGray-500 md:w-2/4"
         type="text"
         v-model="username"
       />
