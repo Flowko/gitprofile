72a19298e4aa66bdd7e66d523e4ad2e4682c4b44
younes <younes.barrad@77diamonds.com>
2022-04-09 23:39:40 +0000

    🪄 added new styles file

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,7 +22,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ["@fortawesome/fontawesome-svg-core/styles.css"],
+  css: ["~/assets/css/main.css"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [{ src: "~/plugins/underscore", ssr: false }],
