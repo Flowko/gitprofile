bc0298d953ef4e7803047cd7df6c5fe93a8cc567
younes <younes.barrad@77diamonds.com>
2022-04-09 23:19:25 +0000

    🚀 updated nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,6 +20,7 @@ export default {
     ],
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
+  target: "static",
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [],
